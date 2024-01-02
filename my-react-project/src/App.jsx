09d8e7c015eaabaa4e6df3ad7277ec829cc6d92d
mariomarquesto <mariomarquesto@gmail.com>
2024-01-02 16:48:06 -0300

    subiendo repositorio

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar1 from "./pages/Navbar"
 import DarkVariantExample from "./pages/Carrousel"
+import Login from "./pages/login";
 
 function App() {
   return <>
   <Navbar1 />
-  <DarkVariantExample />
+  <Login />
   </>;
 }
 
